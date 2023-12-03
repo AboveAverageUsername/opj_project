a74962b52984c11ec251c4d854874b0730b39cf0
Uros Filipovic <uros@programmer.net>
2023-12-03 19:31:15 +0100

    CLS embeddings histogram

---
+++ tensorization scripts/c.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/home/u/anaconda3/envs/cudaGPU/lib:${LD_LIBRARY_PATH}
+python c.py
+
---
+++ tensorization scripts/d.py
@@ -0,0 +1,61 @@
+#!../venv/bin/python3
+#import tensorflow as tf
+#from transformers import BertTokenizer, TFBertModel
+# import torch
+import math
+import os
+import sys
+import numpy as np
+import codecs, json
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+CLS = 101
+SEP = 102
+PAD = 0
+
+# Debug / press any key to continue
+# print("Press ANY key to continue...\n")
+# input()
+
+path = os.path.join("..", "CLSembeddingsAll")
+inFolder = os.listdir(path)
+
+
+
+#tmpFile1 = open(path)
+#line = tmpFile1.readline()
+#tmpFile1.close()
+
+#arr = list(line)
+#=======================================================================================================================
+checkpoint = 0
+current_max = 0
+
+number_of_subs = len(inFolder)
+cls_counts = np.zeros((number_of_subs), dtype=np.int32)
+
+for index, movie in tqdm(enumerate(inFolder), total = number_of_subs, desc = 'Loading CLS counts'):
+    if index < checkpoint:
+        print(f"Skipping to checkpoint {index}\t\t{movie}")
+        continue
+    
+    json_input = open(os.path.join(path, movie), encoding='latin')
+    # L = eval(json_input[1:-2])
+    L = json.load(json_input)
+    array = np.array(L)
+
+    cls_counts[index] = array.shape[0];
+    # print(f"{index} CLS count = {array.shape[0]}\t\t{movie}")
+
+    json_input.close()
+
+cls_max = np.max(cls_counts)
+
+print(f"FINAL RESULT IS {cls_max} CLS TOKENS!!!")
+
+plt.hist(cls_counts, bins=cls_max)
+plt.title("Histogram broja CLS embedinga")
+plt.savefig(os.path.join(".", "hist_cls_counts.pdf"))
+plt.show()
+
