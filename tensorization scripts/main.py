3c8fd93a41bbc1cdb0c0905d6b857f1ffa71ca4d
Uros Filipovic <uros@programmer.net>
2023-12-04 16:42:25 +0100

    kinda working

--- tensorization scripts/main.py
+++ tensorization scripts/main.py
@@ -19,11 +19,12 @@ number_of_chars_in_raiting = 3
 size_of_tensor = 768
 number_of_cls = 40
 
-input_size = number_of_cls * size_of_tensor * 2
+input_size = number_of_cls * size_of_tensor
 
-epoch = 10
+epoch = 10000
 
-dataset_size = 1000
+dataset_size = 10000
+testset_size = 1000
 
 # Some initial setup
 print(f"Is cuda available {torch.cuda.is_available()}")
@@ -121,5 +122,52 @@ for epoch in tqdm(range(epoch), desc = 'Epochs'):
 
         optimizer.step()
 
-print('Done!')
+print('Done training!')
+
+testset_x = []  # ulazni tenzori i att mask
+testset_y = []  # rejting od 0.0 do 1.0
+
+for index in tqdm(range(testset_size), desc = 'Loading testset'):
+    movie = inFolder[dataset_size + index]
+    json_input = open(os.path.join(path, movie), encoding='latin')
+
+    json_load = json.load(json_input)
+    L = np.array(json_load, dtype = np.float32)
+
+    array = np.zeros((input_size), dtype = np.float32)
+
+    cls_count = L.shape[0];
+    # print(f"cls_count {cls_count}")
+
+    # trim the end
+    if cls_count > number_of_cls:
+        cls_count = number_of_cls
+
+    # print(f"cls_count {cls_count}")
+
+    # print(f"type of array {array.shape}")
+    # print(f"type of L {L.shape}")
+    # print(f"cls * sot {cls_count * size_of_tensor}")
+
+    array[0:(cls_count * size_of_tensor)] = L.flatten()[0:(cls_count * size_of_tensor)]
+    
+    # Ucitaj x
+    tensor = torch.from_numpy(array)
+    testset_x.append(tensor)
+
+
+    # Ucitaj y
+    tmp = float(movie[0:number_of_chars_in_raiting])
+    tensor = torch.tensor([tmp], dtype = torch.float32)
+    
+    testset_y.append(tensor)
+
+print("Testing!")
+
+for index in range(testset_size): 
+        x = testset_x[index].cuda()
+        y_pred = model(x)
+
+        y = testset_y[index].cuda()
+        print(f"{y.item()}\t\t{y_pred.item()}")
 
